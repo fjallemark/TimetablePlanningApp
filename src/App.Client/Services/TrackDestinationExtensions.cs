f468b9034a9e600cc326174ff3e2c44ce3bd855f
Stefan Fjällemark <fjallemark@hotmail.com>
2021-10-21 16:04:56 +0200

    Print block destinations in both directions doublesided.

--- src/App.Client/Services/TrackDestinationExtensions.cs
+++ src/App.Client/Services/TrackDestinationExtensions.cs
@@ -4,12 +4,15 @@ namespace Tellurian.Trains.Planning.App.Client.Services
 {
     internal static class TrackDestinationExtensions
     {
-        public static string FlexDirection(this TrainBlocking me) =>
-            me.ReverseBlockOrder() ? "row-reverse" : "row";
+        public static string FlexDirection(this TrainBlocking me, bool reverseItAgain = false) =>
+            me.ReverseBlockOrder(reverseItAgain) ? "row-reverse" : "row";
 
-        public static bool ReverseBlockOrder(this TrainBlocking me) =>
-            me.Train != null && (me.Train.Number % 2 == 0 ? !me.ReverseBlockDestinations :
-            me.ReverseBlockDestinations);
+        private static bool ReverseBlockOrder(this TrainBlocking me, bool reverseItAgain = false) =>
+            me.Train != null && (me.Train.Number % 2 == 0 ? !me.IsBlockOrderReversed(reverseItAgain) :
+            me.IsBlockOrderReversed(reverseItAgain));
+
+        private static bool IsBlockOrderReversed(this TrainBlocking me, bool reverseItAgain = false) =>
+            reverseItAgain ? !me.ReverseBlockDestinations : me.ReverseBlockDestinations;
 
         public static string Display(this TrainInfo me) =>
             $"{me.Prefix} {me.Number}";
